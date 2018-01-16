a9b8feef4c6fa28b9f68f7dfa32096660ade6dfa
Martin Bednorz <mbednorz@ripstech.com>
2018-01-16 16:37:11 +0100

    Add a check before each usage of the RIPS API if the settings are properly stored and return empty data if not

--- ui/RipsModule/src/RipsModule/Controller/WebApiController.php
+++ ui/RipsModule/src/RipsModule/Controller/WebApiController.php
@@ -70,7 +70,15 @@ class WebApiController extends WebAPIActionController {
                 'path' => $app->getInstallPath(),
             ];
         }
-        
+
+        $settings = $this->getLocator()->get('RipsModule\Model\Settings')->getSettings();
+        if (!$this->isConfigurationValid($settings)) {
+            return [
+                'zendApps' => $zendApps,
+                'ripsApps' => [],
+            ];
+        }
+
         $ripsApps = $this->getServiceLocator()->get('RipsModule\Service\RipsApp')->getAll();
 
         return [
@@ -87,6 +95,7 @@ class WebApiController extends WebAPIActionController {
      */
     public function currentDocRootsAction() {
         $this->isMethodGet();
+
         $vhostMapper = $this->getLocator()->get('Vhost\Mapper\Vhost');
 
         try {
@@ -100,15 +109,23 @@ class WebApiController extends WebAPIActionController {
         } catch (\Exception $ex) {
             throw new \Exception(_t('Could not retrieve vhost information'), \Exception::INTERNAL_SERVER_ERROR, $ex);
         }
-        
+
+        $settings = $this->getLocator()->get('RipsModule\Model\Settings')->getSettings();
+        if (!$this->isConfigurationValid($settings)) {
+            return [
+                'vhosts' => $vhosts,
+                'ripsApps' => [],
+            ];
+        }
+
         $ripsApps = $this->getServiceLocator()->get('RipsModule\Service\RipsApp')->getAll();
-        
+
         return [
             'vhosts' => $vhosts,
             'ripsApps' => $ripsApps,
         ];
     }
-    
+
     /**
      * Start a new scan
      *
@@ -151,7 +168,7 @@ class WebApiController extends WebAPIActionController {
             'success' => '1'
         ]);
     }
-    
+
     /**
      * Start a new scan based on a Code Trace
      *
@@ -160,34 +177,34 @@ class WebApiController extends WebAPIActionController {
      */
     public function scanSpecAction() {
         $this->isMethodPost();
-        
+
         // Check the given parameters
         $params = $this->getParameters([
             'vhost_id' => ''
         ]);
-        
+
         $this->validateMandatoryParameters($params, ['vhost_id']);
-        
+
         try {
             $vhostMapper = $this->getLocator()->get('Vhost\Mapper\Vhost');
             $vhost = $vhostMapper->getVhostById($params['vhost_id']);
         } catch (\Exception $ex) {
             throw new \Exception(_t('Could not retrieve vhost information'), \Exception::INTERNAL_SERVER_ERROR, $ex);
         }
-        
+
         $docRoot = $this->getLocator()->get(\RipsModule\Service\DocRoot::class);
         $pathToGuess = $docRoot->getByVhost($vhost);
-        
+
         $scanSpec = $this->getLocator()->get(\RipsModule\Service\ScanSpec::class);
         $content = $scanSpec->getByPath($pathToGuess);
-        
+
         return new WebApiResponseContainer([
             'success' => '1',
             'vendorRemoved' => $scanSpec->isVendorRemoved(),
             'scanSpec' => join("\n", $content)
         ]);
     }
-    
+
     /**
      * Start a new scan based on a Code Trace
      *
@@ -196,7 +213,7 @@ class WebApiController extends WebAPIActionController {
      */
     public function scanDocRootAction() {
         $this->isMethodPost();
-        
+
         // Check the given parameters
         $params = $this->getParameters([
             'rips_id' => 0,
@@ -204,45 +221,45 @@ class WebApiController extends WebAPIActionController {
             'vhost_id' => '',
             'version' => '',
         ]);
-        
+
         $this->validateMandatoryParameters($params, ['rips_id', 'scan_spec', 'vhost_id', 'version']);
         $params['rips_id'] = (int)$params['rips_id'];
-        
+
         if ($params['rips_id'] === 0 || empty($params['scan_spec']) || empty($params['vhost_id']) || empty($params['version'])) {
             throw new \Exception('Data missing');
         }
-        
+
         $this->validateMandatoryParameters($params, ['vhost_id']);
-        
+
         $vhostMapper = $this->getLocator()->get('Vhost\Mapper\Vhost');
-        
+
         try {
             $vhostsResult = $vhostMapper->getVhosts();
             $vhost = $vhostMapper->getVhostById($params['vhost_id']);
         } catch (\Exception $ex) {
             throw new \Exception(_t('Could not retrieve vhost information'), \Exception::INTERNAL_SERVER_ERROR, $ex);
         }
-        
+
         $docRoot = $this->getLocator()->get(\RipsModule\Service\DocRoot::class);
         $parent = $docRoot->getByVhost($vhost);
-        
+
         $filesToScan = explode("\n", $params['scan_spec']);
-        
+
         $zipName = 'rips_' .  $params['rips_id'] . '_' . (new \DateTime())->getTimestamp() . '.zip';
         $zipPath = $this->getLocator()->get(\RipsModule\Service\Zip::class)->create($parent, $filesToScan, $zipName);
-        
+
         $api = $this->getLocator()->get('\RIPS\Api');
-        
+
         try {
             $upload = $api->applications->uploads()->create($params['rips_id'], basename($zipPath), $zipPath);
             $api->applications->scans()->create($params['rips_id'], ['version' => $params['version'], 'upload' => (int)$upload->id]);
         } catch (\Exception $e) {
             throw new \Exception($e->getCode() . ': Starting scan failed: ' . $e->getMessage());
         }
-        
+
         // Remove the temporary archive (was already uploaded)
         unlink($zipPath);
-        
+
         return new WebApiResponseContainer([
             'success' => '1',
             'path' => $zipPath
@@ -258,8 +275,15 @@ class WebApiController extends WebAPIActionController {
     public function scansAction() {
         $this->isMethodGet();
 
+        $settings = $this->getLocator()->get('RipsModule\Model\Settings')->getSettings();
+        if (!$this->isConfigurationValid($settings)) {
+            return new WebApiResponseContainer([
+                'scans' => [],
+                'ui_url' => isset($settings['ui_url']) && !empty($settings['ui_url']) ? $settings['ui_url'] : '',
+            ]);
+        }
+
         $scans = [];
-        
         $api = $this->getLocator()->get('\RIPS\Api');
 
         try {
@@ -267,8 +291,7 @@ class WebApiController extends WebAPIActionController {
         } catch (\Exception $e) {
             throw new \Exception($e->getCode() . ': Getting scans failed: ' . $e->getMessage());
         }
-        
-        $settings = $this->getLocator()->get('RipsModule\Model\Settings')->getSettings();
+
         return new WebApiResponseContainer([
             'scans' => $scans,
             'ui_url' => $settings['ui_url'],
@@ -356,5 +379,20 @@ class WebApiController extends WebAPIActionController {
             'types' => $typeData,
             'ui_url' => $settings['ui_url'],
         ]);
+    }
+
+    /**
+     * Check if the given settings parameter are valid configurations to determine if the user already set-up
+     * the RIPS account.
+     *
+     * @param array $settings
+     * @return boolean
+     */
+    private function isConfigurationValid($settings) {
+        if (!isset($settings['username']) || empty($settings['username']) ||
+            !isset($settings['password']) || empty($settings['password'])) {
+            return false;
+        }
+        return true;
     }
 }
