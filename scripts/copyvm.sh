c446ac0ae92a74ee491c05fd4b2366d4d642ef00
Sönke Tölke <stoelke@ripstech.com>
2019-10-11 14:53:03 +0200

    Remove vm script

--- scripts/copyvm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cp -R ui ~/vm/zend/ui
-cp -R vendor ~/vm/zend/vendor
-cp -R EULA ~/vm/zend/
-cp -R composer.json ~/vm/zend/
-cp -R LICENSE ~/vm/zend/
-cp -R logo.png ~/vm/zend/
-
-
